6feb55f7c7ec6e0f09db43661f1acfb07adfdf21
cheryl-dawn <147960566+cheryl-dawn@users.noreply.github.com>
2024-11-21 13:44:26 -0800

    added first question and answer counter

--- HP_Sorting.cpp
+++ HP_Sorting.cpp
@@ -12,6 +12,28 @@ int main () {
     int answer3;
     int answer4;
 
-    std::cout << "The Sorting Hat Quiz!";
+    std::cout << "The Sorting Hat Quiz!\n" << "\n" << "\n";
+
+    std::cout << "Q1) When I'm dead, I want people to remember me as: \n";
+    std::cout << "\n";
+    std::cout << "1) The Good\n";
+    std::cout << "2) The Great\n";
+    std::cout << "3) The Wise\n";
+    std::cout << "4) The Bold\n" << "\n";
+
+    std::cin >> answer1;
+
+    if (answer1 == 1) {
+        hufflepuff++;
+    } else if (answer1 == 2) {
+        slytherin++;
+    } else if (answer1 == 3) {
+        ravenclaw++;
+    } else if (answer1 == 4) {
+        gryffindor++;
+    } else {
+        std::cout << "Invalid input.";
+    }
+
 
 }
